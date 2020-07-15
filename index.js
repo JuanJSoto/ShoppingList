fa024971b2a87220df4e9a9450f21ce89df561cc
Juan J Soto <jj3185@hotmail.com>
2020-07-14 21:14:42 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+//submit function needed 
+    //what needs to happen, needs to be added to list(append)
+$(function() {
+    $('#.js-shopping-list-form').submit(function(event) {
+        event.preventDefault();
+        const ListItem = $('shopping-list-entry').val();
+
+        $('#shopping-list-entry').val('');
+
+        $(".shopping-list").append(
+            `<li>
+                <span class="shopping-item">${listItem}</span>
+                <div class="shopping-item-controls">
+                    <button class="shopping-item-toggle">
+                        <span class="button-label">check</span>
+                    </button>
+                    <button class="shopping-item-delete">
+                        <span class="button-label">delete</span>
+                    </button>
+                </div>
+            </li>`);
+    });
+
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    });
+
+    $('.shopping-list').on('click', '.shopping-item-delete', function (event) {
+        $(this).closest('li').remove();
+        });
+
+    });
+//delete function to remove item from list (.remove?)
+//if checked, put this class (.shopping-item__checked) 
+
+
+
+
+
+
+
+//$('.shopping-list').empty(); //or .shopping-list-item?
